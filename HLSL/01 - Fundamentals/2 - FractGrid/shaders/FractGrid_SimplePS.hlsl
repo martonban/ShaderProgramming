2e9d80a5908fa3ba6eb604ba308b207d08f221b1
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-09-27 15:06:04 +0200

    WIP

--- HLSL/01 - Fundamentals/2 - FractGrid/shaders/FractGrid_SimplePS.hlsl
+++ HLSL/01 - Fundamentals/2 - FractGrid/shaders/FractGrid_SimplePS.hlsl
@@ -7,8 +7,8 @@ float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
 {
     float cellSize = 50.0;
     float margin = 10.0;
-    float2 cell = fragCoord.xy / (cellSize + margin);
+    float2 cell = fragCoord.xy / cellSize;
+
     float2 t = frac(cell);
-	//float2 q = step(mod(), fragCoord.xy); 
     return float4 (t, 0.0f, 1.0f);
 }
