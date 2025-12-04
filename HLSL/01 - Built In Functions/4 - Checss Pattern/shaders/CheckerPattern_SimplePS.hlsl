77ab99ebf8dbc182fbb9861c15168a0533c44b54
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-12-04 20:52:10 +0100

    rename

--- HLSL/01 - Built In Functions/4 - Checss Pattern/shaders/CheckerPattern_SimplePS.hlsl
+++ HLSL/01 - Built In Functions/4 - Checss Pattern/shaders/CheckerPattern_SimplePS.hlsl
@@ -7,6 +7,7 @@ cbuffer vars : register(b0)
 float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
 {
     float2 uv = fragCoord.xy/uResolution;
+    uv.x *= uResolution.x / uResolution.y;
 	float cx = step(0.5, frac(uv.x * 4.0));
 	float cy = step(0.5, frac(uv.y * 4.0));
 	float ch = abs(cx - cy);
